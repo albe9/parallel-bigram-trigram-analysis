532d5e8b8078f12f570ee9db73cda2efb7e7e9a4
albe9 <albertom1999@gmail.com>
2023-11-23 17:59:38 +0100

    Updated benchmarks graph

--- graph_tracer.py
+++ graph_tracer.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import json
 
 
 def main():
 
-    with open("./media/benchmarks_backup.json",'r') as json_file:
+    # load benchmarks data
+    with open("./media/benchmarks.json",'r') as json_file:
         benchmarks = json.load(json_file)
-        # print(benchmarks)
 
     x_axis = [benchmark["ebook_num"] for benchmark in benchmarks]
     y_axis_seq = [round(sum(benchmark["seq_timings"])/len(benchmark["seq_timings"]), 3) for benchmark in benchmarks]
@@ -15,17 +16,31 @@ def main():
     
     fig, ax = plt.subplots(figsize=(8, 8))
 
-    ax.plot(x_axis, y_axis_seq, label="Sequential")
+    ax.plot(x_axis, y_axis_seq, label="Sequential",)
     ax.plot(x_axis, y_axis_par, label="Parallel")
     ax.plot(x_axis, y_axis_parV2, label="Parallel V2")
-    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
 
-    # Add labels and title
+    # plot vertical lines
+    differences = [(y_seq / y_parV2) for y_seq, y_parV2 in zip(y_axis_seq, y_axis_parV2)]
+
+    for x, y_seq, y_parV2, diff in zip(x_axis, y_axis_seq, y_axis_parV2, differences):
+        ax.plot([x, x], [y_seq, y_parV2], 'r--')
+        ax.text(x + 0.1, (y_seq + y_parV2) / 2, f"{round(diff, 2)}", color='red')
+
+    custom_legend = Line2D([0], [0], color='red', linestyle='--', linewidth=2, markersize=5)
+
     ax.set_xlabel('Number of ebook analysed')
     ax.set_ylabel('Time [s]')
     ax.set_title('Benchmarks')
 
-    ax.legend()
+    # Combine default and custom legends
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles.append(custom_legend)
+    labels.append('Speed_up')
+
+    ax.legend(handles=handles, labels=labels, loc='upper left')
     fig.savefig("./media/benchmarks.png", dpi=600)
     plt.show()
 
