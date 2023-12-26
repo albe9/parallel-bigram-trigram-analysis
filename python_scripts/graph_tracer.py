f375d999e51a94f84f3fbae2249194915a8af525
albe9 <albertom1999@gmail.com>
2023-12-26 20:00:37 +0100

    Added threads graph

--- python_scripts/graph_tracer.py
+++ python_scripts/graph_tracer.py
@@ -4,19 +4,18 @@ import json
 import os
 
 ABS_PATH = os.path.abspath(__file__)
-MEDIA_PATH = f"{os.path.abspath(os.path.join(os.path.dirname(ABS_PATH), os.pardir))}/media"
+MEDIA_MAIN_PATH = f"{os.path.abspath(os.path.join(os.path.dirname(ABS_PATH), os.pardir))}/media"
+MEDIA_PATH_CPP = MEDIA_MAIN_PATH + "/cpp_version"
+MEDIA_PATH_PYTHON = MEDIA_MAIN_PATH + "/python_version"
 
-def main():
-
-    # load benchmarks data
-    with open(f"{MEDIA_PATH}/benchmarks.json",'r') as json_file:
-        benchmarks = json.load(json_file)
-
-    x_axis = [benchmark["ebook_num"] for benchmark in benchmarks]
-    y_axis_seq = [round(sum(benchmark["seq_timings"])/len(benchmark["seq_timings"]), 3) for benchmark in benchmarks]
-    y_axis_par = [round(sum(benchmark["par_timings"])/len(benchmark["par_timings"]), 3) for benchmark in benchmarks]
-    y_axis_parV2 = [round(sum(benchmark["par_timings_V2"])/len(benchmark["par_timings_V2"]), 3) for benchmark in benchmarks]
+def trace_time_ebooks_graph(benchmarks_data):
+    x_axis = [benchmark["ebook_num"] for benchmark in benchmarks_data]
+    y_axis_seq = [round(sum(benchmark["seq_timings"])/len(benchmark["seq_timings"]), 3) for benchmark in benchmarks_data]
+    y_axis_par = [round(sum(benchmark["par_timings"])/len(benchmark["par_timings"]), 3) for benchmark in benchmarks_data]
+    y_axis_parV2 = [round(sum(benchmark["par_timings_V2"])/len(benchmark["par_timings_V2"]), 3) for benchmark in benchmarks_data]
     
+    num_thread = benchmarks_data[0]["threads_num"]
+
     fig, ax = plt.subplots(figsize=(8, 8))
 
     ax.plot(x_axis, y_axis_seq, label="Sequential",)
@@ -36,7 +35,7 @@ def main():
 
     ax.set_xlabel('Number of ebook analysed')
     ax.set_ylabel('Time [s]')
-    ax.set_title('Benchmarks')
+    ax.set_title(f'Benchmarks [{num_thread} threads]')
 
     # Combine default and custom legends
     handles, labels = plt.gca().get_legend_handles_labels()
@@ -44,8 +43,57 @@ def main():
     labels.append('Speed_up')
 
     ax.legend(handles=handles, labels=labels, loc='upper left')
-    fig.savefig(f"{MEDIA_PATH}/benchmarks.png", dpi=600)
-    plt.show()
+    fig.savefig(f"{MEDIA_PATH_CPP}/benchmarks_ebooks.png", dpi=600)
+
+def trace_time_threads_graph(benchmarks_data):
+    x_axis = [benchmark["threads_num"] for benchmark in benchmarks_data]
+    y_axis_seq = [round(sum(benchmark["seq_timings"])/len(benchmark["seq_timings"]), 3) for benchmark in benchmarks_data]
+    y_axis_par = [round(sum(benchmark["par_timings"])/len(benchmark["par_timings"]), 3) for benchmark in benchmarks_data]
+    y_axis_parV2 = [round(sum(benchmark["par_timings_V2"])/len(benchmark["par_timings_V2"]), 3) for benchmark in benchmarks_data]
+    
+    num_ebooks = benchmarks_data[0]["ebook_num"]
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    ax.plot(x_axis, y_axis_seq, label="Sequential",)
+    ax.plot(x_axis, y_axis_par, label="Parallel")
+    ax.plot(x_axis, y_axis_parV2, label="Parallel V2")
+
+    # plot vertical lines
+    differences = [(y_seq / y_par) for y_seq, y_par in zip(y_axis_seq, y_axis_par)]
+
+    for x, y_seq, y_par, diff in zip(x_axis, y_axis_seq, y_axis_par, differences):
+        ax.plot([x, x], [y_seq, y_par], 'r--')
+        ax.text(x + 0.1, (y_seq + y_par) / 2, f"{round(diff, 2)}", color='red')
+
+    custom_legend = Line2D([0], [0], color='red', linestyle='--', linewidth=2, markersize=5)
+
+    ax.set_xlabel('Number of threads utilized')
+    ax.set_ylabel('Time [s]')
+    ax.set_title(f'Benchmarks [{num_ebooks} ebooks]')
+
+    # Combine default and custom legends
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles.append(custom_legend)
+    labels.append('Speed_up')
+
+    ax.legend(handles=handles, labels=labels, loc='lower left')
+    fig.savefig(f"{MEDIA_PATH_CPP}/benchmarks_threads.png", dpi=600)
+
+
+def main():
+
+    # load benchmarks_ebooks data
+    with open(f"{MEDIA_PATH_CPP}/benchmarks_ebooks.json",'r') as json_file:
+        benchmarks_ebooks_data = json.load(json_file)
+    #trace graph
+    trace_time_ebooks_graph(benchmarks_ebooks_data)
+
+    # load benchmarks_threads data
+    with open(f"{MEDIA_PATH_CPP}/benchmarks_threads.json",'r') as json_file:
+        benchmarks_threads_data = json.load(json_file)
+    #trace graph
+    trace_time_threads_graph(benchmarks_threads_data)
 
 if __name__ == "__main__":
     main()
